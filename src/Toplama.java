5740e1e499a39b47fdeb32e4729c778f8d43f6e6
YAHYA <ay.birdeste@gmail.com>
2023-01-27 17:17:46 +0200

    get toplam

---
+++ src/Toplama.java
@@ -0,0 +1,12 @@
+public class Toplama {
+    public static void main(String[] args) {
+
+
+        int sonuc=getToplam(12,45);
+        System.out.println("sonuc = " + sonuc);
+
+    }
+    public static int getToplam(int a,int b){
+        return a+b;
+    }
+}
